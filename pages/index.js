7f146576f2e7e28b478962a6b546c457a1a07c9c
robophil <oghenerobo@gmail.com>
2018-05-18 21:56:29 +0100

    feat: index and server page

---
+++ pages/index.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import axios from 'axios'
+import { ChatFeed, Message } from 'react-chat-ui'
+import Pusher from 'pusher-js'
+import MyChatBubble from '../components/MyChatBubble'
+import '../css/chat.css'
+
+const pusher = new Pusher('key', {
+  cluster: 'eu',
+  encrypted: true
+})
+
+const channel = pusher.subscribe('news')
+
+export default class Index extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      message: '',
+      isTyping: false,
+      messages: [
+        new Message({
+          id: 'bot',
+          message: 'hello',
+          senderName: 'bot'
+        }),
+        new Message({
+          id: 2,
+          message: 'hi bobby'
+        })
+      ]}
+  }
+
+  componentDidMount () {
+    this.receiveUpdateFromPusher()
+  }
+
+  receiveUpdateFromPusher () {
+    channel.bind('news-update', articles => {
+      const messages = articles.map(article => {
+        return new Message({
+          id: 'bot',
+          message: `
+            Description: ${article.title || article.description}
+
+            Url: ${article.url}
+          `
+        })
+      })
+      const intro = messages.length ? 'Here you go, i found some for you' : 'I couldn\'t find any :-(. Search for something else'
+      this.setState({
+        messages: [
+          ...this.state.messages,
+          new Message({
+            id: 'bot',
+            message: intro
+          }),
+          ...messages
+        ]
+      })
+    })
+  }
+
+  handleChange (event) {
+    this.setState({message: event.target.value})
+  }
+
+  handleKeyPress (e) {
+    if (e.key === 'Enter') {
+      const message = e.target.value
+      this.setState({message: ''})
+      this.pushMessage(message)
+
+      axios.post('http://localhost:8080/message', {message})
+      .then(res => {
+        console.log('received by server')
+      })
+      .catch(error => {
+        throw error
+      })
+    }
+  }
+
+  pushMessage (message) {
+    this.setState({
+      messages: [
+        ...this.state.messages,
+        new Message({
+          id: 2,
+          message
+        })
+      ]
+    })
+  }
+
+  render () {
+    return (
+      <div className='chat-wrapper'>
+        <h3> Channel 24: News worldwide with Bobby Mc-newsfeed</h3>
+        <ChatFeed
+          messages={this.state.messages} // Boolean: list of message objects
+          isTyping={this.state.isTyping} // Boolean: is the recipient typing
+          hasInputField={false} // Boolean: use our input, or use your own
+          showSenderName // show the name of the user who sent the message
+          bubblesCentered={false} // Boolean should the bubbles be centered in the feed?
+          chatBubble={MyChatBubble}
+          maxHeight={750}
+        />
+        <input className='chat-input' type='text' value={this.state.message}
+          onChange={this.handleChange.bind(this)}
+          onKeyPress={this.handleKeyPress.bind(this)}
+          placeholder='Type a message...' />
+      </div>
+    )
+  }
+}
---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Pusher = require('pusher')
+const countryData = require('country-data')
+const dialogflow = require('dialogflow')
+const NewsAPI = require('newsapi')
+const newsapi = new NewsAPI('89d35166eca048379723cb4bc283fa8f')
+
+const projectId = 'news-42175'
+const sessionId = 'a-random-session-id'
+const languageCode = 'en-US'
+
+const sessionClient = new dialogflow.SessionsClient()
+const sessionPath = sessionClient.sessionPath(projectId, sessionId)
+
+const countryDataByName = countryData.countries.all.reduce((acc, curr) => {
+  acc[curr.name.toLowerCase()] = curr
+  return acc
+}, {})
+
+const buildQuery = function (query) {
+  return {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: languageCode
+      }
+    }
+  }
+}
+
+const fetchNews = function (intentData) {
+  const {source, category} = intentData
+  const geoCountry = intentData['geo-country']
+  const sourceString = source.listValue.values.map(value => value.stringValue.toLowerCase())
+
+  return newsapi.v2.topHeadlines({
+    // sources: sourceString.join(','),
+    category: category.stringValue,
+    language: 'en',
+    country: countryDataByName[geoCountry.stringValue.toLowerCase()] ? countryDataByName[geoCountry.stringValue.toLowerCase()].alpha2 : 'us'
+  })
+}
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 8080
+
+const pusher = new Pusher({
+  appId: 'app_id',
+  key: 'key',
+  secret: 'secret',
+  cluster: 'cluster',
+  encrypted: true
+})
+
+app.post('/message', function (req, res) {
+  return sessionClient
+  .detectIntent(buildQuery(req.body.message))
+  .then(responses => {
+    console.log('Detected intent')
+    const result = responses[0].queryResult
+    const intentData = responses[0].queryResult.parameters.fields
+
+    if (result.intent) {
+      return fetchNews(intentData)
+      .then(news => news.articles)
+      .then(articles => pusher.trigger('news', 'news-update', articles.splice(0, 6)))
+      .then(() => console.log('published to pusher'))
+    } else {
+      console.log(`  No intent matched.`)
+    }
+    return res.sendStatus(200)
+  })
+  .catch(err => {
+    console.error('ERROR:', err)
+  })
+})
+
+app.listen(port, function () {
+  console.log('Node app is running at localhost:' + port)
+})
